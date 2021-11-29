4170e0ac94a53ea3bb0e77c3d7fb44b793374298
DioenDJS <negociospacheti@gmail.com>
2021-11-29 11:21:21 -0300

    :recycle: change config knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
+    connection: {
+      host:      process.env.DB_HOST,
+      database:  process.env.DB_BANCO,
+      user:      process.env.DB_USER,
+      password:  process.env.DB_PASS
+    },
     url: process.env.DATABASE_URL,
     migrations: {
       tableName: 'migrations',

da879c4fe1096e93f28ba109e53dcae09ae7eb4a
DioenDJS <negociospacheti@gmail.com>
2021-12-05 02:42:22 -0300

    :recycle: change config in file knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,15 @@
-// Update with your config settings.
-require('dotenv').config();
+
 module.exports = {
 
   development: {
     client: 'pg',
-  connection: process.env.DATABASE_URL,
-  searchPath: ['knex', 'public'],
+    connection: {
+      host:     '127.0.0.1',
+      database: 'camiseta',
+      user:     'postgres',
+      password: 'docker',
+      port: '5433'
+    },
     migrations: {
       tableName: 'migrations',
       directory: 'database/migrations'

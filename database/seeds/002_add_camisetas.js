59d40ee3157bc8f8d03b9f4bfe4c75fbd61c08cb
DioenDJS <negociospacheti@gmail.com>
2021-12-05 02:47:32 -0300

    :recycle: change value field marca_id

--- database/seeds/002_add_camisetas.js
+++ database/seeds/002_add_camisetas.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('camisetas').insert([
-        {modelo: 'verde_copy', marca_id: 5, ano: 2020, preco: 5999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/1/u/1up-cam-masculina-verde_copy_.jpg'},
-        {modelo: 'deadpool', marca_id: 2, ano: 2020, preco: 7999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/b/i/bigdeadpool-feminina-vermelho_copy_.jpg'},
-        {modelo: 'blackphanter', marca_id: 3, ano: 2020, preco: 3999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/b/l/blackphanter-masculina-preto_copy__1.jpg'}
+        {modelo: 'verde_copy', marca_id: '35', ano: 2020, preco: 5999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/1/u/1up-cam-masculina-verde_copy_.jpg'},
+        {modelo: 'deadpool', marca_id: '34', ano: 2020, preco: 7999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/b/i/bigdeadpool-feminina-vermelho_copy_.jpg'},
+        {modelo: 'blackphanter', marca_id: '36', ano: 2020, preco: 3999, foto: 'https://www.cz10.com.br/media/catalog/product/cache/1/small_image/330x330/9df78eab33525d08d6e5fb8d27136e95/b/l/blackphanter-masculina-preto_copy__1.jpg'}
       ]);
     });
 };

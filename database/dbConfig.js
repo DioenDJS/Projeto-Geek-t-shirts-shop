263ec1eba3e5a86caceb1992bb41b06310375733
DioenDJS <negociospacheti@gmail.com>
2021-12-05 02:45:53 -0300

    :recycle: change config database in file dbConfig.js

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
 const knex = require('knex')({
-  client: process.env.DB_CLIENT,
+  client: 'pg',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_BANCO
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'docker',
+    database : 'camiseta',
+    port: '5433'
   }
 });
 
